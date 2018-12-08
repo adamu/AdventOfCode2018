9663646c13461525a797c9884c02920836f42888
Adam Millerchip <adam@millerchip.net>
2018-12-09 01:23:59 +0900

    Add Day 5 Part 1

---
+++ day5/day5.exs
@@ -0,0 +1,25 @@
+defmodule Day5 do
+  def upcase?(char), do: char in ?A..?Z
+  def same_letter?(c1, c2), do: String.upcase(<<c1::utf8>>) === String.upcase(<<c2::utf8>>)
+  def case_differs?(c1, c2), do: upcase?(c1) !== upcase?(c2)
+  def react?(c1, c2), do: same_letter?(c1, c2) && case_differs?(c1, c2)
+
+  # note: reverses the list - for part 1 doesn't seem to matter
+  def react(chars) do
+    Enum.reduce(chars, [], fn
+      curr, [] -> [curr]
+      curr, [prev | old] -> if react?(curr, prev), do: old, else: [curr, prev | old]
+    end)
+  end
+
+  def part1 do
+    File.read!("input")
+    |> String.trim()
+    |> String.to_charlist()
+    |> react()
+    |> Enum.count()
+    |> IO.puts()
+  end
+end
+
+Day5.part1()
