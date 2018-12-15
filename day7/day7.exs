77d2bb3c4db0fd715e1fc50305949fbaf7295b20
Adam Millerchip <adam@millerchip.net>
2018-12-15 21:56:28 +0900

    Add Day 7 Part 2

--- day7/day7.exs
+++ day7/day7.exs
@@ -43,7 +43,83 @@ defmodule Day7 do
     resolve_steps(Map.delete(graph, next), [next | steps])
   end
 
+  def time_for(step), do: hd(String.to_charlist(step)) - 5
+
+  def get_work({graph, counts, in_progress}) do
+    case Enum.filter(counts, fn {_, count} -> count === 0 end)
+         |> Enum.map(fn {step, _count} -> step end)
+         |> Enum.sort() do
+      [] ->
+        {nil, {graph, counts, in_progress}}
+
+      [step | _] ->
+        {{step, time_for(step)}, {graph, Map.delete(counts, step), [step | in_progress]}}
+    end
+  end
+
+  def get_work_done(completed_step, {graph, _counts, in_progress}) do
+    graph = Map.delete(graph, completed_step)
+    counts = count_predecessors(graph) |> Map.drop(in_progress)
+    get_work({graph, counts, in_progress})
+  end
+
+  def all_prerequisites_allocated?(graph, in_progress) do
+    map_size(Map.drop(graph, in_progress)) === 0
+  end
+
+  def tick(workers, graph, counts, in_progress, seconds) do
+    {workers, {graph, counts, _}} =
+      Enum.map_reduce(workers, {graph, counts, in_progress}, fn
+        nil, acc -> get_work(acc)
+        {step, 0}, acc -> get_work_done(step, acc)
+        {step, remaining}, acc -> {{step, remaining - 1}, acc}
+      end)
+
+    {workers, graph, counts, seconds + 1}
+  end
+
+  def finish_up(workers, counts, seconds) do
+    remaining_workers =
+      workers
+      |> Enum.filter(fn worker -> worker != nil end)
+      |> Enum.map(fn {_, time} -> time end)
+      |> Enum.max()
+
+    # This assumes the number of remaining jobs will not be greater than the remaining
+    # workers. That's probably a bad assumption so if the answer is wrong I'll
+    # come back and fix this.
+    if map_size(counts) > length(workers) do
+      raise("Number of final jobs greater than remaining workers")
+    end
+
+    remaining_queued = (counts |> Enum.map(fn {step, _} -> time_for(step) end) |> Enum.max()) + 1
+
+    seconds + remaining_workers + remaining_queued
+  end
+
+  def work(workers, graph, counts, seconds) do
+    in_progress =
+      workers
+      |> Enum.filter(fn worker -> worker != nil end)
+      |> Enum.map(fn {step, _} -> step end)
+
+    if all_prerequisites_allocated?(graph, in_progress) do
+      finish_up(workers, counts, seconds)
+    else
+      {workers, graph, counts, seconds} = tick(workers, graph, counts, in_progress, seconds)
+      work(workers, graph, counts, seconds)
+    end
+  end
+
   def part1, do: input_to_graph() |> resolve_steps([]) |> Enum.join()
+
+  def part2 do
+    graph = input_to_graph()
+    counts = count_predecessors(graph)
+    workers = for _ <- 1..5, do: nil
+    work(workers, graph, counts, 0)
+  end
 end
 
 IO.puts(Day7.part1())
+IO.puts(Day7.part2())
