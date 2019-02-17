e0b9657c2cf696291e1c9d34ed375fe6f3b5516a
Adam Millerchip <adam@millerchip.net>
2019-02-17 20:28:12 +0900

    Time passes... Completely rework Day 12 Part 1

--- day12/day12.exs
+++ day12/day12.exs
@@ -6,103 +6,65 @@ defmodule Day12 do
 
     def parse(file) do
       IO.read(file, String.length("initial state: "))
-      state = file |> IO.read(:line) |> String.trim() |> str_to_bits()
+      state = file |> IO.read(:line) |> String.trim() |> String.to_charlist() |> Enum.with_index()
       IO.read(file, :line)
       {state, parse_spec(file)}
     end
 
     def parse_spec(file), do: parse_spec(file, IO.read(file, :line), %{})
-
     def parse_spec(_file, :eof, spec), do: spec
 
     def parse_spec(file, line, spec) do
-      [<<left::5>>, <<right::1>>] =
-        ~r/(.{5}) => (.)/
-        |> Regex.run(line, capture: :all_but_first)
-        |> Enum.map(&str_to_bits/1)
+      [pattern, <<result::utf8>>] = ~r/(.{5}) => (.)/ |> Regex.run(line, capture: :all_but_first)
 
-      parse_spec(file, IO.read(file, :line), Map.put(spec, left, right))
-    end
-
-    def str_to_bits(str) do
-      str
-      |> String.graphemes()
-      |> Enum.reduce(<<>>, fn char, bits -> <<bits::bits, convert_char(char)::bits>> end)
-    end
-
-    def convert_char("#"), do: <<1::1>>
-    def convert_char("."), do: <<0::1>>
-  end
-
-  defmodule Bitstring do
-    @moduledoc "Helper module for visualising bitstrings"
-    def visualise(bits), do: visualise(bits, "")
-    def visualise(<<>>, str), do: str
-    def visualise(<<0::1, rest::bits>>, str), do: visualise(rest, str <> "0")
-
-    # Not sure which of these is faster, will trust to_string over n concatenations
-    # def visualise(<<1::1, rest::bits>>, str), do: visualise(rest, str <> "1")
-    def visualise(bits, str) do
-      size = bit_size(bits)
-      <<int::size(size)>> = bits
-      str <> Integer.to_string(int, 2)
+      parse_spec(
+        file,
+        IO.read(file, :line),
+        Map.put(spec, String.to_charlist(pattern), result)
+      )
     end
   end
 
-  # Ensure intial two pots are empty
-  def generation(<<1::1, rest::bits>>, spec), do: generation(<<1::5, rest::bits>>, spec)
-  def generation(<<1::2, rest::bits>>, spec), do: generation(<<1::5, rest::bits>>, spec)
+  def evolve(input, _spec, 0), do: input
 
-  # Entry point (after handing special case above)
-  def generation(bits, spec), do: generation(bits, <<>>, spec)
-
-  # Terminating condition
-  def generation(<<>>, next, _spec), do: trim(next)
-
-  # Process pattern of 5
-  def generation(<<pattern::5, _rest::bits>> = thisgen, nextgen, spec) do
-    generation(thisgen, nextgen, pattern, spec)
+  def evolve(input, spec, generations) do
+    input = generation(input, spec)
+    evolve(input, spec, generations - 1)
   end
 
-  # Pad with zeros for end of input
-  def generation(thisgen, nextgen, spec) do
-    padding = 5 - bit_size(thisgen)
-    <<pattern::5>> = <<thisgen::bits, 0::size(padding)>>
-    generation(thisgen, nextgen, pattern, spec)
+  def generation(input, spec) do
+    input |> pad |> propagate(spec)
   end
 
-  # Consumes an element of thisgen and adds an element to nextgen
-  def generation(thisgen, nextgen, pattern, spec) do
-    <<_::1, thisgen::bits>> = thisgen
-    generation(thisgen, <<nextgen::bits, spec[pattern] || 0::1>>, spec)
+  def propagate([a, b, c, d, e | rest], spec) do
+    {_, pot} = c
+    pattern = visualise([a, b, c, d, e])
+    [{Map.get(spec, pattern), pot} | propagate([b, c, d, e | rest], spec)]
   end
 
-  def trim(<<0::1, rest::bits>>), do: trim(rest)
+  def propagate(rest, _spec) when length(rest) == 4, do: []
 
-  def trim(bits) do
-    size = bit_size(bits) - 1
-
-    case bits do
-      <<trimed::bits-size(size), 0::1>> -> trim(trimed)
-      bits -> bits
-    end
+  # Pads with 4 empty pots. If the spec contained ..... => #, would need to pad with 5
+  def pad(list) do
+    list |> Enum.reverse() |> pad_rear |> Enum.reverse() |> pad_front
   end
 
-  def spread(pots, _spec, 0) do
-    IO.puts("20: #{Bitstring.visualise(pots)}")
-    pots
+  def pad_front([{?., _}, {?., _}, {?., _}, {?., _} | _] = done), do: done
+  def pad_front([{_, i} | _] = list), do: pad_front([{?., i - 1} | list])
+
+  def pad_rear([{?., _}, {?., _}, {?., _}, {?., _} | _] = done), do: done
+  def pad_rear([{_, i} | _] = list), do: pad_rear([{?., i + 1} | list])
+
+  def visualise(input) do
+    Enum.map(input, fn {x, _i} -> x end)
   end
 
-  def spread(pots, spec, times) do
-    IO.puts("#{20 - times}: #{Bitstring.visualise(pots)}")
-    spread(generation(pots, spec), spec, times - 1)
-  end
+  def part1 do
+    {input, spec} = Parser.parse("input")
 
-  # FUUUU got the generations, but apparently needed to keep track of the indecies.
-  # Will need to undo the "trim" feature and work out how to keep the array stable
-  # Or maybe can just keep track of how many I added to the left, which will be minus indeces
-  def part1(filename) do
-    {pots, spec} = Parser.parse(filename)
-    spread(pots, spec, 20)
+    evolve(input, spec, 20)
+    |> Enum.filter(fn {plant, _} -> plant === ?# end)
+    |> Enum.map(fn {_, pot} -> pot end)
+    |> Enum.sum()
   end
 end
